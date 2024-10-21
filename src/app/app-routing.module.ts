3bfc8fcc7e1d70f2fa4ae13416edaff9262b5e81
hugocg21 <hugocastanogonzalez@gmail.com>
2024-10-21 16:41:36 +0200

    App files updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssistanceComponent } from './components/assistance/assistance.component';
+import { PlayersListComponent } from './components/players-list/players-list.component';
+import { TrainingCalendarComponent } from './components/training-calendar/training-calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'assistance', component: AssistanceComponent },
+  { path: 'players-list', component: PlayersListComponent },
+  { path: 'training-calendar', component: TrainingCalendarComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AssistanceComponent } from './components/assistance/assistance.component';
+import { TrainingCalendarComponent } from './components/training-calendar/training-calendar.component';
+import { FormsModule } from '@angular/forms';
+import { PlayersListComponent } from './components/players-list/players-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    AssistanceComponent,
+    TrainingCalendarComponent,
+    PlayersListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
