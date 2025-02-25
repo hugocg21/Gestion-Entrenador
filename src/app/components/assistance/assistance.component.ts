9b302aecd4004ad1d5168414cece4aaacfe5ccfc
hugocg21 <130666286+hugocg21@users.noreply.github.com>
2025-02-25 13:15:39 +0100

    Update assistance.component.ts

--- src/app/components/assistance/assistance.component.ts
+++ src/app/components/assistance/assistance.component.ts
@@ -83,6 +83,7 @@ export class AssistanceComponent implements OnInit {
       new Date(2025, 0, 3), // 3 de enero
       new Date(2025, 0, 17), // 17 de enero
       new Date(2025, 1, 14), // 14 de febrero
+      new Date(2025, 1, 21), // 21 de febrero
     ];
 
     if (currentMonth === 8 && currentYear === 2024) {
